d0338fa11624a81ad002eadd37cae923937def3d
millen0 <millenomatos@icloud.com>
2026-03-18 19:12:35 -0300

    Fix API URLs to use HTTPS instead of HTTP

--- frontend/backoffice/src/services/api.js
+++ frontend/backoffice/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://status.piercloud.com/api';
+const API_URL = 'https://status.piercloud.com/api';
 
 export const api = axios.create({
   baseURL: API_URL,
