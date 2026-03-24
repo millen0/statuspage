8a2f451f3cabe5de3328ac151609689cd5e1da1b
millen0 <millenomatos@icloud.com>
2026-03-24 11:31:33 -0300

    fix: add cache busting to vite build

--- frontend/public-page/vite.config.js
+++ frontend/public-page/vite.config.js
@@ -5,5 +5,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
   }
 })
